f97f3d97456fa0066b7c25cd5fb0d3f12ba3ed72
testlnord <testlnord@gmail.com>
2014-04-28 22:32:05 +0400

    fingers typing words

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ __author__ = 'arkady'
 import src.gdbrer
 import src.log_parser
 import config
-
+from src.graph.graph import drawLines
 
 def main():
     log = src.gdbrer.gdblog(config.def_bin, config.gdb_script, config.logs_path)
